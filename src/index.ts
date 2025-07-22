58d333192726b0a53ce62c64f654b86fa0179602
Amin Abbaspour <amin.abbaspour@okta.com>
2025-07-22 11:23:57 +1000

    proxy working fine

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,23 @@ app.onError((err, c) => {
   return c.text(`Internal Server Error: ${err.message}`, 500);
 });
 
+app.all('/*', async (c) => {
+
+  const request = new Request(c.req.raw);
+  const url = new URL(request.url);
+  //console.log(`proxying req to url: ${url.pathname} to hostname: ${c.env.AUTH0_EDGE_LOCATION}`);
+
+  url.hostname = c.env.AUTH0_EDGE_LOCATION;
+  request.headers.set('cname-api-key', c.env.CNAME_API_KEY);
+
+  return await fetch(url, request);
+})
+
+/*
 app.get('/', async (c) => {
   return c.text(`federation migration. cname-api-key: ${c.env.CNAME_API_KEY}, edge-location: ${c.env.AUTH0_EDGE_LOCATION}`);
 });
+*/
 
 // noinspection JSUnusedGlobalSymbols
 export default app;
